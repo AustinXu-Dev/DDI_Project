d673a27021152bace7ddb705880a1a970ccb932c
Skypoppi <austin.xu.me@gmail.com>
2024-03-07 22:55:49 +0700

    Initial Commit

---
+++ DDI_Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DDI_Project
+//
+//  Created by Austin Xu on 2024/3/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DDI_Project/DDI_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDI_ProjectApp.swift
+//  DDI_Project
+//
+//  Created by Austin Xu on 2024/3/7.
+//
+
+import SwiftUI
+
+@main
+struct DDI_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
